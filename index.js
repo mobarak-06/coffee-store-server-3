d3c3186aede08b0b0e299392d43a13b6c731eba1
Mobarak Hossain <mobarakhossain121838@gmail.com>
2024-09-13 20:29:46 +0600

    new uri added

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ajv51.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASS);
+
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dyqxkty.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -61,10 +65,14 @@ async function run() {
           taste: coffee.taste,
           category: coffee.category,
           details: coffee.details,
-          photo: coffee.photo
+          photo: coffee.photo,
         },
       };
-      const result = await coffeeCollection3.updateOne(filter, updatedCoffee, options);
+      const result = await coffeeCollection3.updateOne(
+        filter,
+        updatedCoffee,
+        options
+      );
       res.send(result);
     });
 
@@ -76,41 +84,39 @@ async function run() {
       res.send(result);
     });
 
-
     // user related api
 
-    app.get("/users", async(req, res) => {
+    app.get("/users", async (req, res) => {
       const cursor = userCollection.find();
       const result = await cursor.toArray();
       res.send(result);
-    })
+    });
 
-    app.post("/users", async(req, res) => {
-      const newUser =  req.body;
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
       const result = await userCollection.insertOne(newUser);
       res.send(result);
+    });
 
-    })
-
-    app.patch("/users", async(req, res) => {
+    app.patch("/users", async (req, res) => {
       const user = req.body;
-      const filter = {email: user.email}
+      const filter = { email: user.email };
       const updatedDoc = {
-        $set:{
-          lastLoggedIn : user.lastLoggedIn
-        }
-      }
+        $set: {
+          lastLoggedIn: user.lastLoggedIn,
+        },
+      };
       const result = await userCollection.updateOne(filter, updatedDoc);
-      res.send(result); 
-    })
+      res.send(result);
+    });
 
-    app.delete("/users/:id", async(req, res) => {
+    app.delete("/users/:id", async (req, res) => {
       const id = req.params.id;
       console.log(id);
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await userCollection.deleteOne(query);
       res.send(result);
-    })
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
